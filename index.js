2787989d6024e39f1d21907be31ade38e2ae7cb7
Bernard Brown <browb130@mail.broward.edu>
2022-01-30 22:22:18 -0500

    Fixed main bug that allowed for previous set operator to be changed.

--- index.js
+++ index.js
@@ -36,13 +36,18 @@ let display = document.getElementById("display-digits")
 // Create function to set display numbers depending on whether a num-input was pressed yet.
 // Also if user attempts to enter second decimal it is removed with slice.
 let setDisplay = inp => {
-    numInpBool = true
-    console.log(numInpBool)
-    display.textContent == "0" ? display.textContent = inp.target.textContent : display.textContent += inp.target.textContent
-    if (display.textContent.split(".").length - 1 > 1) {
-        display.textContent = display.textContent.slice(0, -1)
+    // If statement prevents user from entering inputs after sum is displayed with equal sign.
+    if (previousOperation !== "equal") {
+        numInpBool = true
+        console.log(numInpBool)
+        display.textContent == "0" ? display.textContent = inp.target.textContent : display.textContent += inp.target.textContent
+        // If statement to prevent extra decimal points from being placed when there is already one present.
+        if (display.textContent.split(".").length - 1 > 1) {
+            display.textContent = display.textContent.slice(0, -1)
+        }
     }
 }
+
 let sum = ""
 // Create function to set display to first "num-input" after an operator is selected
 let resetAfterOperator = inp => {
@@ -65,8 +70,7 @@ buttons.clear.addEventListener("click", () => {
     prevNumber = 0
     operators.forEach(el => el.classList.remove("active"))
     sum = ""
-
-
+    previousOperation = ""
 })
 
 // Add eventListener to "clear-entry" to remove most recent input.
@@ -80,18 +84,24 @@ buttons.clearEntry.addEventListener("click", () => {
 let setActive = inp => {
     document.querySelector(".active") == null ? false : document.querySelector(".active").classList.remove("active")
     inp.target.classList.add("active")
+    // If statement to check if prevNumber and display.textContent both exist.
+    // If so the calc function is called to evaluate the sum of the operands.
+    // Also prevNumber is set to "" to avoid duplicate calls of calc function.
+    if (prevNumber && display.textContent) {
+        calc(prevNumber, display.textContent)
+        prevNumber = ""
+    }
+    activeOperator = inp.target.dataset.operator
 }
+
 let operators = document.querySelectorAll(".operator")
 // Foreach method to add eventlistener to all operator buttons
-operators.forEach(el => el.addEventListener("click", setActive))
-
-
-
+operators.forEach(el => {
+    el.addEventListener("click", setActive)
+})
 
 let calc = (prevNum, curNum) => {
-    if (document.querySelector(".active") !== null) {
-        let activeOperator = document.querySelector(".active").dataset.operator
-        currOperator = activeOperator
+    if (activeOperator !== null) {
         if (prevNum && display.textContent && boolean == false && numInpBool == true) {
             switch (activeOperator) {
                 case "/": console.log("divide")
@@ -133,73 +143,20 @@ let calc = (prevNum, curNum) => {
     }
 }
 
-let currOperator = document.querySelector("#equals").dataset.operator
-let equivalent = (prevNum, curNum) => {
-    if (prevNum && display.textContent && numInpBool == true) {
-        switch (currOperator) {
-            case "/": console.log("divide")
-                if (sum != "") {
-                    sum = (parseFloat(sum) / parseFloat(curNum))
-                    display.textContent = sum
-                }
-                sum = (parseFloat(prevNum) / parseFloat(curNum))
-                display.textContent = sum
-                break;
-            case "*": console.log("multi")
-                if (sum != "") {
-                    sum = (parseFloat(sum) * parseFloat(curNum))
-                    display.textContent = sum
-                }
-                sum = (parseFloat(prevNum) * parseFloat(curNum))
-                display.textContent = sum
-                break;
-            case "-": console.log("subtract")
-                if (sum != "") {
-                    sum = (parseFloat(sum) - parseFloat(curNum))
-                    display.textContent = sum
-                }
-                sum = (parseFloat(prevNum) - parseFloat(curNum))
-                display.textContent = sum
-                break;
-            case "+":
-                if (sum != "") {
-                    sum = (parseFloat(sum) + parseFloat(curNum))
-                    display.textContent = sum
-                }
-                sum = (parseFloat(prevNum) + parseFloat(curNum))
-                display.textContent = sum
-                // Prevent imprecise calculation
-                if (sum === 0.30000000000000004) {
-                    display.textContent = .3
-                }
-                break;
-            default:
-                console.log(`sorry we dont have this ${currOperator}`)
-        }
-    }
-}
-
-// Create boolean that will be on if the last input was a number.
+// Create boolean that will be "on" if the last input was a number.
 // If last input was not a number the boolean is set to false to prevent consecutive operator and equal clicks from creating bug.
 let numInpBool = ""
 // Create boolean variable to toggle on and off depending on what eventlistener calls it.
 // If anything other than equal calls an event listener it will be false to prevent consecutive equal clicks from creating bug.
 let boolean = ""
 let prevNumber = display.dataset.previousNum
-
-// POSSIBLY DELETE LATER
-// buttons.addition.addEventListener("click", () => {
-//     calc(prevNumber, display.textContent)
-//     numInpBool = false
-//     boolean = false
-//     // Set previous number to current display.textContent so it can be used in future operation
-//     prevNumber = display.textContent
-// })
+let previousOperation = display.dataset.previousOperation
 
 buttons.equals.addEventListener("click", () => {
-    equivalent(prevNumber, display.textContent)
+    calc(prevNumber, display.textContent)
     numInpBool = false
     boolean = true
+    previousOperation = "equal"
 })
 
 operators.forEach(el => {
@@ -209,5 +166,7 @@ operators.forEach(el => {
         boolean = false
         // Set previous number to current display.textContent so it can be used in future operation
         prevNumber = display.textContent
+        previousOperation = "operator"
     })
 })
+
